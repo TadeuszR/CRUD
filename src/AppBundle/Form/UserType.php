d1917bbfbcdf625d7716fac67f6b655d946506b5
Tadeusz Raczek <traczek@vivalavita.pl>
2016-12-01 17:15:37 +0100

    User Entity and CRUD

---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('surname')->add('telephone')->add('address')        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'appbundle_user';
+    }
+
+}
